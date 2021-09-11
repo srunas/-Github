f2e86ec13e1b4276df8b4257fd27346918e7a80a
Dima Strunevsiy <89925844+srunas@users.noreply.github.com>
2021-09-11 16:37:19 +0500

    Program V.2
    Было добавлено зацикливание для работы программы с помощью Python. Так же функции : Умножить , Разделить, для удобного восприятия информации был сделан перевод на следующую строку с помощью: /n.

--- Программа для освоения Github.py
+++ Программа для освоения Github.py
@@ -1,6 +1,9 @@
-import math
-x = int(input('Введите x: '))
-y = int(input('Введите y: '))
-print(x+y, x-y, (abs(x)+abs(y))/2)
+while 1:
+    import math
+
+    x = int(input('Введите x: '))
+    y = int(input('Введите y: '))
+    print(x + y, x - y, (abs(x) + abs(y)) / 2, x * y, x / y, sep='\n')
+
 
 
